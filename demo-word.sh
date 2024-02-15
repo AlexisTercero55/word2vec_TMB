3e7773c9952606dc1270e99d684422a8fc4af577
Alexis Tercero <87354316+AlexisTercero55@users.noreply.github.com>
2024-02-15 13:48:05 -0600

    FIX | infiny loop c-cpp workflow update demo-word.sh

--- demo-word.sh
+++ demo-word.sh
@@ -4,4 +4,4 @@ if [ ! -e text8 ]; then
   gzip -d text8.gz -f
 fi
 time ./word2vec -train text8 -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
-./distance vectors.bin
+# ./distance vectors.bin
